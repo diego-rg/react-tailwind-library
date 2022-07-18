a37ca013ffca9079ad06e0b9534b2727183c8312
eu <diego_thegarden@hotmail.com>
2022-07-18 22:31:24 +0200

    button lg

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./index.css";
 
 import Button from "./components/Button";
 import ButtonOutlined from "./components/ButtonOutlined";
+import ButtonLarge from "./components/ButtonLarge";
 
 function App() {
   return (
     <div className="App">
       <Button />
       <ButtonOutlined />
+      <ButtonLarge />
     </div>
   );
 }
---
+++ src/components/ButtonLarge.jsx
@@ -0,0 +1,9 @@
+const ButtonLarge = () => {
+  return (
+    <button className="px-3 py-2 m-2 rounded-md font-medium uppercase bg-red-600 hover:bg-red-500 text-white border border-transparent">
+      Large Button
+    </button>
+  );
+};
+
+export default ButtonLarge;
