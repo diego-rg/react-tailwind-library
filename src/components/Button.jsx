1e4268f7630316a128733a571f5f26333cb4bb80
eu <diego_thegarden@hotmail.com>
2022-07-18 22:22:49 +0200

    buttons

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="px-2.5 py-1.5 m-2 rounded-md font-medium bg-blue-700 hover:bg-blue-600 text-white border border-transparent">
+      Button
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/ButtonOutlined.jsx
@@ -0,0 +1,9 @@
+const ButtonOutlined = () => {
+  return (
+    <button className="px-2.5 py-1.5 m-2 rounded-md font-medium bg-transparent hover:bg-transparent text-black border border-black">
+      Button
+    </button>
+  );
+};
+
+export default ButtonOutlined;
