173868e091d5e4215c13558841ded23289d24856
eu <diego_thegarden@hotmail.com>
2022-07-23 17:29:31 +0200

    a href navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,10 +12,18 @@ const Navbar = () => {
     <nav className="flex items-center md:h-20 h-16 px-4 text-white bg-black">
       <h1 className="w-full md:text-3xl text-xl font-bold text-white">Brand</h1>
       <ul className="hidden md:flex">
-        <li className="p-4 text-lg">Home</li>
-        <li className="p-4 text-lg">Services</li>
-        <li className="p-4 text-lg">About</li>
-        <li className="p-4 text-lg">Contact</li>
+        <li className="p-4 text-lg">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p-4 text-lg">
+          <a href="#home">Services</a>
+        </li>
+        <li className="p-4 text-lg">
+          <a href="#home">About</a>
+        </li>
+        <li className="p-4 text-lg">
+          <a href="#home">Contact</a>
+        </li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
         {nav ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
@@ -31,10 +39,18 @@ const Navbar = () => {
           Brand
         </h1>
         <ul className="uppercase p-2">
-          <li className="p-4 border-b border-b-gray-400">Home</li>
-          <li className="p-4 border-b border-b-gray-400">Services</li>
-          <li className="p-4 border-b border-b-gray-400">About</li>
-          <li className="p-4 border-b border-b-gray-400">Contact</li>
+          <li className="p-4 border-b border-b-gray-400">
+            <a href="#home">Home</a>
+          </li>
+          <li className="p-4 border-b border-b-gray-400">
+            <a href="#home">Services</a>
+          </li>
+          <li className="p-4 border-b border-b-gray-400">
+            <a href="#home">About</a>
+          </li>
+          <li className="p-4 border-b border-b-gray-400">
+            <a href="#home">Contact</a>
+          </li>
         </ul>
       </div>
     </nav>
